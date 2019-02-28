179b1f1898737cdf80208d69aeed796753ba32b2
Josh Krumm <josh_krumm@abercrombie.com>
2019-02-27 23:34:00 -0500

    Updates

--- src/components/media/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from "./media.module.css";
-
-const Media = (props) => (
-    <img className={`${styles.image} ${props.className}`} src={props.src} alt={props.altText}/>
-);
-
-export default Media;
--- src/pages/contactus.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Layout from '../layouts';
-import grid from "../layouts/grid.module.css";
-
-const ContactUs = () => (
-	<Layout>Under Construction</Layout>
-);
-
-export default ContactUs;
