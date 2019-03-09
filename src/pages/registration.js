f8f45e139b8c862be4d4cfa985f43e1b4dd3c4f1
jskrumm <jskrumm@gmail.com>
2019-03-08 20:28:39 -0500

    Added 2019 registration info

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -3,7 +3,7 @@ import Layout from '../layouts';
 
 const Registration = () => (
     <Layout>
-        <iframe src="https://www.eventbrite.com/e/2018-ohio-mens-ox-roast-and-retreat-registration-42194086649?ref=eweb" frameborder="0" height="1000" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+        <iframe src="https://www.eventbrite.com/e/2019-ohio-mens-ox-roast-and-retreat-registration-58464581136" frameborder="0" height="1000" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
     </Layout>
 );
 
